b98868c240806dfe6d8a2f65a6c54996bc341eba
Seman Said <seman@users.noreply.github.com>
2016-03-08 19:09:27 -0800

    Added Datastore object.

--- cwrstatus/datastore.py
+++ cwrstatus/datastore.py
@@ -2,7 +2,51 @@ from ConfigParser import ConfigParser
 import os
 
 from boto.s3.connection import S3Connection
+import pymongo
 
+from cwrstatus.config import ds
+from cwrstatus.utils import get_current_utc_time
+
+
+class Datastore:
+
+    def __init__(self, collection='cwr', limit=20):
+        self.collection = ds.db[collection]
+        self.limit = limit
+
+    def get(self, filter=None, limit=None, skip=0, latest_first=True):
+        """ Return documents from the datastore.
+
+        :param filter: dict specifying the filter elements.
+        :param limit:  The maximum number of results to return.
+        :param skip: The number of documents to omit.
+        :param latest_first: Last in first out.
+        :return:
+        """
+        limit = limit or self.limit
+        sort = [('_updated_on', pymongo.DESCENDING)] if latest_first else None
+        return self.collection.find(
+            filter=filter, limit=limit, skip=skip, sort=sort)
+
+    def get_by_bundle_name(self, bundle_name, limit=None, skip=0):
+        """ Return documents filtered by bundle name.
+
+        See self.get() parameters for more info.
+        """
+        filter = ({'bundle_name': bundle_name})
+        return self.get(filter=filter, limit=limit, skip=skip)
+
+    def update(self, _id, doc):
+        """Update a document(s) in this collection.
+
+         If the document doesn't exist, it will be created. If it exists, it
+         will be updated.
+        :param _id: Document id.
+        :param doc: Document data.
+        :return:
+        """
+        doc['_updated_on'] = get_current_utc_time()
+        return self.collection.replace_one(_id, doc, upsert=True)
 
 class S3:
 
@@ -62,6 +106,7 @@ class S3:
             raise ValueError(
                 'Key was not found from the path: {}'.format(path))
         return key
+
 
 
 def get_s3_access():
