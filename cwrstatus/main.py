75f59038d2aad944bb83c6f200abc527d9ad170d
Seman Said <seman@users.noreply.github.com>
2016-03-10 18:22:38 -0800

    Added static and templates.

--- cwrstatus/main.py
+++ cwrstatus/main.py
@@ -1,18 +1,31 @@
-from cwrstatus.config import app, ds
+from datetime import datetime
 
+from flask import render_template
+
+from cwrstatus.config import app
 from cwrstatus.import_data import from_s3
+from cwrstatus.datastore import Datastore
 
 
 @app.route('/')
 def index():
-    user = ds.db.users.find_one({'_id': 'id'})
-    return 'hello {}'.format(user)
+    ds = Datastore()
+    cwr_results = ds.get()
+    return render_template('base.html', cwr_results=cwr_results)
 
 
 @app.route('/import-data')
 def import_data():
     from_s3()
     return 'done.'
+
+
+@app.template_filter('humanize_date')
+def humanize_date_filter(value, time_format=None):
+    iso_time_format = "%Y-%m-%dT%H:%M:%S"
+    time_format = time_format or iso_time_format
+    value = datetime.strptime(value, time_format)
+    return value.strftime("%b %d, %Y at %H:%M")
 
 
 if __name__ == '__main__':
